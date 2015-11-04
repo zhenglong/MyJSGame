6437eb68119f1776281077a48a642a87367e4ac5
zhenglong <tomorrow_is_good@hotmail.com>
2015-11-04 18:00:12 +0800

    add scenes

--- src/app.js
+++ src/app.js
@@ -1,3 +1,144 @@
+var Scene1 = cc.Scene.extend({
+	ctor: function() {
+		this._super();
+		var gate = new cc.Sprite(res.gate_bg_png);
+		gate.setAnchorPoint(0, 0);
+		this.addChild(gate, 2);
+		var door = new cc.Sprite(res.door_jpg);
+		door.setScale(.6, .3);
+		door.setAnchorPoint(-1, -.4);
+		this.addChild(door, 1);
+		var listener = cc.EventListener.create({
+			event: cc.EventListener.TOUCH_ONE_BY_ONE,
+			swallowTouches: true,
+			onTouchBegan: function(touch, event) {
+				var target = event.getCurrentTarget();
+				var locationInNode = target.convertToNodeSpace(touch.getLocation());
+
+				var s = target.getContentSize();
+				var rect = cc.rect(0, 0, s.width, s.height);
+
+				if (cc.rectContainsPoint(rect, locationInNode)) {
+					var action = cc.moveBy(.5, cc.p(0, door.getBoundingBox().height));
+					var sequence = cc.sequence([action, cc.callFunc(function() {
+						cc.director.runScene(new Scene2());
+					}, this)]);
+					door.runAction(sequence);
+					return true;
+				}
+				return false;
+			}
+		});
+		cc.eventManager.addListener(listener, door);
+		cc.log('scene1');
+	},
+	onEnter: function() {
+		this._super();
+		cc.log('scene1 enter');
+	},
+	onExit: function() {
+		this._super();
+		cc.log('scene1 exit');
+	}
+});
+
+var Scene2 = cc.Scene.extend({
+	ctor: function() {
+		this._super();
+		this.addChild(new Background2Layer());
+		this.addChild(new MascotLayer());
+		this.addChild(new ConversationLayer());
+		var listener = cc.EventListener.create({
+			event: cc.EventListener.TOUCH_ONE_BY_ONE,
+			swallowTouches: true,
+			onTouchBegan: function(touch, event) {
+				cc.director.runScene(new Scene3());
+				return true;
+			}
+		});
+		cc.eventManager.addListener(listener, this);
+		cc.log('scene2');
+	},
+	onEnter: function() {
+		this._super();
+		cc.log('scene2 enter');
+	},
+	onExit: function() {
+		this._super();
+		cc.log('scene2 exit');
+	}
+});
+
+var Background2Layer = cc.Layer.extend({
+	ctor: function() {
+		this._super();
+		// TODO: add a background image
+	}
+});
+
+var MascotLayer = cc.Layer.extend({
+	ctor: function() {
+		this._super();
+
+		var size = cc.winSize;
+		var mascot = new cc.Sprite(res.mascot_png);
+		mascot.setScale(.15);
+		mascot.setPosition(cc.p(40, size.height - 30));
+		this.addChild(mascot);
+	}
+});
+
+var ConversationLayer = cc.Layer.extend({
+	ctor: function() {
+		this._super();
+		var size = cc.winSize;
+		var txt = new cc.DrawNode();
+		txt.drawRect(cc.p(40, size.height / 2 - 10), cc.p(size.width - 40, size.height - 60), 
+			new cc.Color(0, 0, 0, 255), 2, new cc.Color(255, 255, 255));
+		this.addChild(txt);
+		var label = new cc.LabelTTF('很久很久以前有一个故事。。。', 'Arial', 20);
+		label.fillStyle = new cc.Color(255, 255, 255);
+		label.setPosition(cc.p(size.width / 2 + 10, size.height - 90));
+		label.boundingWidth = size.width - 80;
+		//label.boundingHeight = size.height / 2 - 50;
+		this.addChild(label);
+	}
+});
+
+var Scene3 = cc.Scene.extend({
+	ctor: function() {
+		this._super();
+		var size = cc.winSize;
+		var boxes = new BoxesTiledMap();
+		var bound = boxes.boundingBox();
+		boxes.setPosition((size.width -bound.width) / 2, 
+			(size.height - bound.height) / 2);
+		this.addChild(boxes);
+		var mapSize = boxes.getMapSize();
+		var layer = boxes.getLayer('bg');
+		var listener = cc.EventListener.create({
+			event: cc.EventListener.TOUCH_ONE_BY_ONE,
+			swallowTouches: true,
+			onTouchBegan: function(touch, event) {
+				// get touched tile 
+				var tile = layer.getTileAt(layer.convertTouchToNodeSpace(touch));
+				if (tile) {
+					cc.log('tile is touched');
+				}
+				return true;
+			}
+		});
+		cc.eventManager.addListener(listener, boxes);
+		cc.log('scene3');
+	}
+});
+
+var BoxesTiledMap = cc.TMXTiledMap.extend({
+	ctor: function() {
+		this._super();
+		this.initWithTMXFile(res.boxes_tmx);
+	}
+});
 
 var HelloWorldLayer = cc.Layer.extend({
 	sprite1: null,
--- src/resource.js
+++ src/resource.js
@@ -2,7 +2,14 @@ var res = {
     HelloWorld_png : "res/HelloWorld.png",
 	cyanSquare_png: 'res/cyanSquare.png',
 	magentaSquare_png: 'res/magentaSquare.png',
-	yellowSquare_png: 'res/yellowSquare.png'
+	yellowSquare_png: 'res/yellowSquare.png',
+	gate_bg_png: 'res/gate_bg_1.png',
+	door_jpg: 'res/door.jpg',
+	mascot_png: 'res/mascot.png',
+	box_close_png: 'res/box-close.png',
+	box_open_empty_png: 'res/box-open-empty.png',
+	box_open_full_png: 'res/box-open-full.png',
+	boxes_tmx: 'res/boxes.tmx'
 };
 
 var g_resources = [];
